e7e1b69adb16d712b344475a5d0c4f3327406e24
Belpaire <belpaire.bram@student.kuleuven.be>
2019-02-28 19:00:36 +0100

    Signed-off-by: Belpaire <belpaire.bram@student.kuleuven.be>

---
+++ references/picture.py
@@ -0,0 +1,7 @@
+class Picture:
+    # True for horizontal orientation, false otherwise
+    def __init__(self,ishorizontal,nbtags,tags):
+        self.ishorizontal = ishorizontal
+        self.nbtags = nbtags
+        self.tags = tags
+
