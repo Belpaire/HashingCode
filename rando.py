81e329c0d7a1ab998e4bd8fa6d6fe4de0fe262cf
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2019-02-28 19:16:28 +0100

    split files

--- rando.py
+++ rando.py
@@ -1,14 +1,26 @@
 
 
-file_name = "a_example.txt"
+file_name = "input/d_pet_pictures.txt"
+
+file_v = "stef/d_pet_pictures_v.txt"
+file_h = "stef/d_pet_pictures_h.txt"
 
 def main():
 
-    f = open(file_name, 'r')
+    f = open(file_name, "r")
+    v = open(file_v, "w")
+    h = open(file_h, "w")
 
-    f.readline()
+    nb = int(f.readline())
 
-    
+    for i in range(nb):
+        line = f.readline()
+        content = line.split()
+
+        if content[0] == "H":
+            h.write(line)
+        else:
+            v.write(line)
 
 if __name__ == '__main__':
     main()
