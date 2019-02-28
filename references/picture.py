5280262d159e9173b50baf82c74ef5ed6db394f4
Belpaire <belpaire.bram@student.kuleuven.be>
2019-02-28 19:01:51 +0100

    Signed-off-by: Belpaire <belpaire.bram@student.kuleuven.be>

--- references/picture.py
+++ references/picture.py
@@ -1,6 +1,10 @@
 class Picture:
     # True for horizontal orientation, false otherwise
-    def __init__(self,ishorizontal,nbtags,tags):
+    # id = unique number
+    #nbtags should be equal to length tags
+    #tags is a list of tags
+    def __init__(self,id,ishorizontal,nbtags,tags):
+        self.id = id
         self.ishorizontal = ishorizontal
         self.nbtags = nbtags
         self.tags = tags
